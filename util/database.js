d137d6ec93f79564f6ba98c98e59bb2d5d2ec6f7
gaurav404 <gaurav.agarwala19@gmail.com>
2019-04-26 19:13:16 +0530

    deploy test

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool(process.env.NODE_ENV==='production'?require('./prod_database.js'):require('./dev_database.js'));
+const pool = mysql.createPool(require('./dev_database.js'));
 
 module.exports = pool.promise();
