c9628c4b312db666a9a003372a3e57c310574e61
gaurav404 <gaurav.agarwala19@gmail.com>
2019-04-26 14:34:06 +0530

    prepared for deploy

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./util/database');
+const path = require('path');
 const app = express();
 const public = require('./routes/public');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+}
 app.use(public);
 app.listen(5000);
