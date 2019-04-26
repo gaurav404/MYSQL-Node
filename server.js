3264a5b7c9368f5db766aa2865975efb4bac46c7
gaurav404 <gaurav.agarwala19@gmail.com>
2019-04-26 15:36:02 +0530

    server.js lines placements

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 const public = require('./routes/public');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(public);
 if(process.env.NODE_ENV==='production'){
   app.use(express.static('client/build'));
   app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   });
 }
-app.use(public);
 app.listen(5000);
