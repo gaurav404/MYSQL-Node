a712f4d4a1d4ed357ce7e443e9827a343b219261
gaurav404 <gaurav.agarwala19@gmail.com>
2019-04-26 14:16:52 +0530

    production and development database

--- util/database.js
+++ util/database.js
@@ -1,14 +1,9 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'db-intern.ciupl0p5utwk.us-east-1.rds.amazonaws.com',
-  user: 'dummyUser',
-  database: 'db_intern',
-  password: 'dummyUser01',
-  port:3306
-});
+const pool = mysql.createPool(process.env.NODE_ENV==='production'?require('./prod_database.js'):require('./dev_database.js'));
 
 module.exports = pool.promise();
+
 /*
 host: 'db-intern.ciupl0p5utwk.us-east-1.rds.amazonaws.com',
 user: 'dummyUser',
---
+++ util/prod_database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: proccess.env.HOST,
+    user: proccess.env.USER,
+    database: proccess.env.DATABASE,
+    password: proccess.env.PASSWORD,
+    port:proccess.env.PORT
+}
